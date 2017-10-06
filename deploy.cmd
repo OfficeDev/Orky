6b838345d1288e3eb2eedcd7d60168ce4d4e83b9
Slawek Zachcial <slawek.zachcial@gmail.com>
2017-10-06 09:19:51 +0200

    Fix TypeScript transpiling errors during deployment
    TypeScript transpiling invoked during deploymet (`deploy.cmd`) fails due
    to missing dependencies defined in `devDependencies` section of
    `package.json`. Removing `--production` causes `npm install` to pull all
    the dependencies, fixing transpiling errors.
    
    Resolves #15

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --production
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
